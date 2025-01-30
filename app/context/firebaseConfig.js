30fd205ef9f8e91b537edca307868121a2ff87f2
crcristian97 <crcristian97.cc@gmail.com>
2025-01-30 20:32:54 -0300

    add env

--- app/context/firebaseConfig.js
+++ app/context/firebaseConfig.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB3HBuQIZqtBhO15_uOU2fKdDlk3rEjtHs",
-  authDomain: "nextjs-firebase-883d2.firebaseapp.com",
-  projectId: "nextjs-firebase-883d2",
-  storageBucket: "nextjs-firebase-883d2.firebasestorage.app",
-  messagingSenderId: "740501768918",
-  appId: "1:740501768918:web:5682bec8ebddcbeaacd777",
-  measurementId: "G-RHBMKZKDV6"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
 };
 
 let app;
